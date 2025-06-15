3e5b3c2a68916d9c2b69e4ef7e33a60d494da85c
Yagyesh Jha <133356984+Yagyeshjha@users.noreply.github.com>
2025-06-15 21:08:11 +0530

    Update variable.tf
    comented all code

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
-variable "access_key" {
-  default = "AGA"
-  }
-variable "secret_key" {
-  default = ""
-  }
-variable "region" {
-  default = "ap-south-1"
-  }
+//variable "access_key" {
+//  default = "AGA"
+//  }
+//variable "secret_key" {
+ // default = ""
+//  }
+//variable "region" {
+ // default = "ap-south-1"
+  //}//
